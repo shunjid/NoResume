0bb12e475e1d74d181d340e731f57f4bf0187991
shunjid <shunjid1862@gmail.com>
2020-01-12 16:13:04 +0600

    upgraded shortbios form inside modal

--- wwwroot/api/shortBiosValidator.js
+++ wwwroot/api/shortBiosValidator.js
@@ -1,26 +1,25 @@
-var transactionId;
-$(document).ready(function(){
+$(document).ready(function () {
     $('.modal').modal();
-    
-    
-    var postLoader = $('#postLoader');
+    let postLoader = $('#postLoader');
     postLoader.hide();
 
-    $("#BioEditorForm").submit(function(e){
+    $("#BioEditorForm").submit(function (e) {
         e.preventDefault();
         postLoader.show();
 
         $.post('', $('#BioEditorForm').serialize(), function (response) {
-            if(response != null || response !== "" || response !== "null"){
+            if (response != null || response !== "" || response !== "null") {
+                let DevJobAvailability = $("#DevJobAvailability");
                 M.toast({
                     html: 'Data updated Successfully',
                     classes: 'green darken-1 rounded'
                 });
-                $('#DevCurrentCity').text(response.currentCity);
-                $('#DevShortDescription').html(response.shortDescription);
-                $('input[name=PhoneNumber]').val(response.developerPhoneNumber);
-                true === response.isAvailableForJob ? $("#DevJobAvailability").text("Yes"):$("#DevJobAvailability").text("No");
-            }else{
+                $('#modalEditShortBio').modal('close');
+                $('#DevCurrentCity').text(response["currentCity"]);
+                $('#DevShortDescription').html(response["shortDescription"]);
+                $('input[name=PhoneNumber]').val(response["developerPhoneNumber"]);
+                true === response["isAvailableForJob"] ? DevJobAvailability.text("Yes") : DevJobAvailability.text("No");
+            } else {
                 M.toast({
                     html: 'Something went wrong !',
                     classes: 'red darken-4 rounded'
@@ -30,68 +29,63 @@ $(document).ready(function(){
         });
     });
 
-    
-    $("#subscriptionForm").submit(function(e){
+
+    $("#subscriptionForm").submit(function (e) {
         e.preventDefault();
-        var phonenumber = $("#PhoneNumber").val();
-        if(phonenumber.length != 11)
-        {
+        let phonenumber = $("#PhoneNumber").val();
+        if (phonenumber.length !== 11) {
             M.toast({
                 html: 'Number must be 11 digit !',
                 classes: 'red darken-4'
             });
-        }
-        else if (phonenumber.startsWith("017") || phonenumber.startsWith("013"))
-        {
+        } else if (phonenumber.startsWith("017") || phonenumber.startsWith("013")) {
             $("#otpForm").hide();
             $("#agreement").show();
             $('.modal').modal('open');
-            $("#acceptbtn").click(function(){
-                
+            $("#acceptbtn").click(function () {
+
                 $("#agreement").hide();
                 $("#otpForm").show();
 
-                phonenumber = "88"+phonenumber;
-                $.post('/ShortBios/createOTP', {phonenumber : phonenumber}, function (responseData) {
-                    
+                
+                phonenumber = "88" + phonenumber;
+                $.post('/ShortBios/createOTP', {phonenumber: phonenumber}, function (responseData) {
+
                     // nothing 
- 
-                }).done(function (xhr, status){
-                   // nothing
+
+                }).done(function (xhr, status) {
+                    // nothing
                 });
-          
+
             });
 
-            $("#rejectbtn").click(function(){
+            $("#rejectbtn").click(function () {
                 $('.modal').modal('close');
             });
-        }
-        else
-        {
+        } else {
             M.toast({
                 html: 'You have to use GP number !',
                 classes: 'red darken-4'
             });
         }
     });
-        
-        $("#otpForm").submit(function (e) {
-           e.preventDefault();
-           var tpin = $("#otp").val();
-         
-            $.post('/ShortBios/chargeOTP', {tpin : tpin}, function (otpdata) {
 
-            }).done(function (xhr, status){
-                // nothing
-                location.reload();
-                
-             }).fail(function (xhr, status){
-                // nothing
-                M.toast({
-                    html: 'Incorrent OTP number !',
-                    classes: 'red darken-4'
-                });
-             });;
+    $("#otpForm").submit(function (e) {
+        e.preventDefault();
+        let tpin = $("#otp").val();
+
+        $.post('/ShortBios/chargeOTP', {tpin: tpin}, function (otpdata) {
+            console.log(otpdata);
+        }).done(function () {
+            // nothing
+            location.reload();
+        }).fail(function () {
+            // nothing
+            M.toast({
+                html: 'Incorrect OTP number !',
+                classes: 'red darken-4'
+            });
         });
+    });
 });
 
